f18a9c8ded25e613a7433b9c029a33f7d66d3efc
msyk <nii@msyk.net>
2020-11-08 15:29:40 +0900

    Update again,formula for submit

--- fmcsadmin.rb
+++ fmcsadmin.rb
@@ -5,8 +5,24 @@ class Fmcsadmin < Formula
   sha256 "e9dbe141b38812b13d227c145f3c7c2e5d5af24f500f81f897227acfe56a3784"
   license "MIT License"
 
+  depends_on "go" => :build
+
   def install
     command = "fmcsadmin"
-    bin.install command
+    version = "1.1.0"
+    
+    builddir = "dist/#{command}-#{version}-macos"
+    buildresult = "#{builddir}/#{command}"
+    system "go", "get", "-u", "github.com/mattn/go-scan"
+    system "go", "get", "-u", "github.com/olekukonko/tablewriter"
+    system "go", "get", "-u", "golang.org/x/crypto/ssh/terminal"
+    system "mkdir", "-p", builddir
+    system "GOOS=darwin", "GOARCH=amd64", "CGO_ENABLED=0", "go", "build", "-ldflags", "-X main.version=#{version}", "-o", buildresult
+    bin.install buildresult
   end
+
+  test do
+    system "go", "test"
+  end
+
 end
