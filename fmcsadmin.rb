e9ac79371b52a41231d00555ecde8a06b7693dca
msyk <nii@msyk.net>
2020-11-08 13:10:27 +0900

    Use variables and bug fix

--- fmcsadmin.rb
+++ fmcsadmin.rb
@@ -8,13 +8,18 @@ class Fmcsadmin < Formula
   depends_on "go" => :build
 
   def install
+    command = "fmcsadmin"
+    version = "1.1.0"
+    
+    builddir = "dist/#{command}-#{version}-macos"
+    buildresult = "#{builddir}/#{command}"
     system "go", "get", "-u", "github.com/mattn/go-scan"
     system "go", "get", "-u", "github.com/olekukonko/tablewriter"
     system "go", "get", "-u", "golang.org/x/crypto/ssh/terminal"
 #    system "make", "build-macos"
-    system "mkdir", "-p", "dist/fmcsadmin-1.1.0-macos"
-    system "GOOS=darwin", "GOARCH=amd64", "CGO_ENABLED=0", "go", "build", "-ldflags", "-X main.version=1.1.0", "-o", "dist/fmcsadmin-1.1.0-macos/fmcsadmin"
-    bin.install "dist/macos/fmcsadmin"
+    system "mkdir", "-p", builddir
+    system "GOOS=darwin", "GOARCH=amd64", "CGO_ENABLED=0", "go", "build", "-ldflags", "-X main.version=#{version}", "-o", buildresult
+    bin.install buildresult
   end
 
   test do
