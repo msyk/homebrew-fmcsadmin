b83c4e002dc1214e7ab9dd7c66a9b180c72f503b
msyk <nii@msyk.net>
2020-11-08 15:49:36 +0900

    Back to built package

--- fmcsadmin.rb
+++ fmcsadmin.rb
@@ -1,28 +1,12 @@
 class Fmcsadmin < Formula
   desc "The 'fmcsadmin' is the command-line tool to administrate FileMaker Server via FileMaker Admin API."
   homepage "https://github.com/emic/fmcsadmin"
-  url "https://github.com/emic/fmcsadmin/archive/1.1.0.tar.gz"
-  sha256 "e6bf17371f241616ef1168f912b29376922595821fa74dea00d98614cf300b0b"
+  url "https://github.com/emic/fmcsadmin/releases/download/1.1.0/fmcsadmin-1.1.0-macos.zip"
+  sha256 "e9dbe141b38812b13d227c145f3c7c2e5d5af24f500f81f897227acfe56a3784"
   license "MIT License"
 
-  depends_on "go" => :build
-
   def install
-    command = "fmcsadmin"
-    version = "1.1.0"
-    
-    builddir = "dist/#{command}-#{version}-macos"
-    buildresult = "#{builddir}/#{command}"
-    system "go", "get", "-u", "github.com/mattn/go-scan"
-    system "go", "get", "-u", "github.com/olekukonko/tablewriter"
-    system "go", "get", "-u", "golang.org/x/crypto/ssh/terminal"
-    system "mkdir", "-p", builddir
-    system "GOOS=darwin", "GOARCH=amd64", "CGO_ENABLED=0", "go", "build", "-ldflags", "-X main.version=#{version}", "-o", buildresult
-    bin.install buildresult
-  end
-
-  test do
-    system "go", "test"
+    bin.install fmcsadmin
   end
 
 end
