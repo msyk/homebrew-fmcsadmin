825eb8dc48b617790c5f53bca01c1fa1c943cb17
msyk <nii@msyk.net>
2021-02-17 12:43:06 +0900

    Update for v.1.2

--- fmcsadmin.rb
+++ fmcsadmin.rb
@@ -1,8 +1,8 @@
 class Fmcsadmin < Formula
   desc "The 'fmcsadmin' is the command-line tool to administrate FileMaker Server via FileMaker Admin API."
   homepage "https://github.com/emic/fmcsadmin"
-  url "https://github.com/emic/fmcsadmin/releases/download/1.1.0/fmcsadmin-1.1.0-macos.zip"
-  sha256 "e9dbe141b38812b13d227c145f3c7c2e5d5af24f500f81f897227acfe56a3784"
+  url "https://github.com/emic/fmcsadmin/releases/download/1.2.0/fmcsadmin-1.2.0-macos.zip"
+  sha256 "ace2d270fd5a28ef33f734d4fc0afc10880c8731d5b8effedbeb34ec073496f2"
   license "MIT License"
 
   def install
